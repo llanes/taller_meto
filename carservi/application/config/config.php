ff5c68982371fcc4010c770aef676451492c6f48
llanes villagra <milciades.anals@msn.com>
2015-09-08 09:58:49 -0400

    sumari

--- carservi/application/config/config.php
+++ carservi/application/config/config.php
@@ -44,7 +44,7 @@ else
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = 'index.php/Home';
 
 /*
 |--------------------------------------------------------------------------
